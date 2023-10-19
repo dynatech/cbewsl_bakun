b9b1bd59c1c4e7756ebbbac44c5a73cf9df96140
David <david063095@gmail.com>
2023-10-19 18:37:10 +0800

    updated variables

--- src/apis/SurficialMeasurements.js
+++ src/apis/SurficialMeasurements.js
@@ -1,34 +1,41 @@
-import { API_URL } from '../config'
-import axios from 'axios'
+import { API_URL } from "../config";
+import axios from "axios";
+import { CBEWSL_SITE } from "../host";
 
 export const getSurficialData = (data, callback) => {
-    axios.get(`${API_URL}/api/surficial/get_surficial_plot_data/mar/${data.startDate}/${data.endDate}`).then((response) => {
-        callback(response.data)
-    }).catch((error) => {
-
-    });
-}
+  axios
+    .get(
+      `${API_URL}/api/surficial/get_surficial_plot_data/${CBEWSL_SITE}/${data.startDate}/${data.endDate}`
+    )
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch((error) => {});
+};
 
 export const sendMeasurement = (data, callback) => {
-    axios.post(`${API_URL}/api/surficial/insert_web`, data).then((response) => {
-        callback(response.data)
-    }).catch((error) => {
-
-    });
-}
+  axios
+    .post(`${API_URL}/api/surficial/insert_web`, data)
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch((error) => {});
+};
 
 export const deletePrevMeasurement = (data, callback) => {
-    axios.post(`${API_URL}/api/surficial/delete_prev_measurement`, { mo_id: data }).then((response) => {
-        callback(response.data)
-    }).catch((error) => {
-
-    });
-}
+  axios
+    .post(`${API_URL}/api/surficial/delete_prev_measurement`, { mo_id: data })
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch((error) => {});
+};
 
 export const getStaffs = (callback) => {
-    axios.get(`${API_URL}/api/misc/get_all_staff_users`).then((response) => {
-        callback(response.data)
-    }).catch((error) => {
-    
-    });
-}
+  axios
+    .get(`${API_URL}/api/misc/get_all_staff_users`)
+    .then((response) => {
+      callback(response.data);
+    })
+    .catch((error) => {});
+};
