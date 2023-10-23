2ebd869c5549a3ab01e9a4700bdec6218b23b52c
jcdeguzman <jeremiah.dg17@gmail.com>
2023-10-23 11:05:00 +0800

    +added new logos and fixed sign in page

--- src/components/utils/SignInLogo.js
+++ src/components/utils/SignInLogo.js
@@ -4,10 +4,10 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import phivolcs_seal from "../../assets/phivolcs_seal.png";
 import dynaslope_seal from "../../assets/dynaslope_seal.png";
-import ilolo_province_seal from "../../assets/iloilo_province_seal.png";
-import leon_municipal_seal from "../../assets/leon_municipal_seal.png";
+import benguet_province_seal from "../../assets/benguet_province_seal.png";
+import bak_municipal_seal from "../../assets/bakun_municipal_seal.png";
 import leon_mdrrmc_responder from "../../assets/leon_mdrrmc_responder.png";
-import mar_lewc_seal from "../../assets/mar_lewc_seal.png";
+import lewc_seal from "../../assets/bak_lewc_seal.png";
 
 const useStyles = makeStyles((theme) => ({
     md_image: {
@@ -40,17 +40,17 @@ function SignInLogo(props) {
                         <img className={classes.md_image} src={dynaslope_seal} />
                     </Grid>
                     <Grid item xs={1} md={1}>
-                        <img className={classes.md_image} src={ilolo_province_seal} />
+                        <img className={classes.md_image} src={benguet_province_seal} />
                     </Grid>
                     <Grid item xs={1} md={1}>
-                        <img className={classes.md_image} src={leon_municipal_seal} />
+                        <img className={classes.md_image} src={bak_municipal_seal} />
                     </Grid>
-                    <Grid item xs={1} md={1}>
+                    {/* <Grid item xs={1} md={1}>
                         <img className={classes.md_image} src={leon_mdrrmc_responder} />
-                    </Grid>
-                    <Grid item xs={1} md={1}>
+                    </Grid> */}
+                    {/* <Grid item xs={1} md={1}>
                         <img className={classes.md_image} src={mar_lewc_seal} />
-                    </Grid>
+                    </Grid> */}
                 </Grid>    
             </Grid>
         </Container>
